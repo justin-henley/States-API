98838d103d747886e701e3d495096197f433b9c5
Justin Henley <justin.a.henley@gmail.com>
2022-04-21 02:56:21 +0800

    Added routes for state, capital, population, nickname, and admission

--- routes/api/states.js
+++ routes/api/states.js
@@ -2,8 +2,25 @@ const express = require('express');
 const router = express.Router();
 const statesController = require('../../controllers/statesController');
 
-router.route('/').get(statesController.getAllStates).post().patch().delete();
+// Returns data for all states
+router.route('/').get(statesController.getAllStates);
 
-// Other routes like :state go here
+// Returns all data for a single state
+router.route('/:state').get(statesController.getState);
+
+// Returns a state name and capital city
+router.route('/:state/capital').get(statesController.getStateCapital);
+
+// Returns a state name and nickname
+router.route('/:state/nickname').get(statesController.getStateNickname);
+
+// Returns a state name and population
+router.route('/:state/population').get(statesController.getStatePopulation);
+
+// Returns a state name and admission date
+router.route('/:state/admission').get(statesController.getStateAdmission);
+
+// State fun facts
+router.route('/:state/funfact').get().post().patch().delete();
 
 module.exports = router;
