82d7efdcbd8bc2ebf5ba8bfe69c900e82083dded
Justin Henley <justin.a.henley@gmail.com>
2022-04-21 00:53:01 +0800

    State model fleshed out

--- model/State.js
+++ model/State.js
@@ -2,7 +2,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const stateSchema = new Schema({
-  // TODO
+  // The two-char code for the state
+  statecode: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  // An array of fun facts about the state
+  funfacts: {
+    type: [String],
+  },
 });
 
 module.exports = mongoose.model('State', stateSchema);
