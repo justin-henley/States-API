ced03f19c46fac1f9e6ed6a9ddac0db8f5a9f248
Justin Henley <justin.a.henley@gmail.com>
2022-04-24 22:30:15 +0800

    added verification middleware

--- routes/api/states.js
+++ routes/api/states.js
@@ -2,24 +2,32 @@ const express = require('express');
 const router = express.Router();
 const statesController = require('../../controllers/statesController');
 const funfactsController = require('../../controllers/funfactsController');
-
+const verifyState = require('../../middleware/verifyState');
 // Returns data for all states
 router.route('/').get(statesController.getAllStates);
 
 // Returns all data for a single state
-router.route('/:state').get(statesController.getState);
+router.route('/:state').get(verifyState, statesController.getState);
 
 // Returns a state name and capital city
-router.route('/:state/capital').get(statesController.getStateCapital);
+router
+  .route('/:state/capital')
+  .get(verifyState, statesController.getStateCapital);
 
 // Returns a state name and nickname
-router.route('/:state/nickname').get(statesController.getStateNickname);
+router
+  .route('/:state/nickname')
+  .get(verifyState, statesController.getStateNickname);
 
 // Returns a state name and population
-router.route('/:state/population').get(statesController.getStatePopulation);
+router
+  .route('/:state/population')
+  .get(verifyState, statesController.getStatePopulation);
 
 // Returns a state name and admission date
-router.route('/:state/admission').get(statesController.getStateAdmission);
+router
+  .route('/:state/admission')
+  .get(verifyState, statesController.getStateAdmission);
 
 // State fun facts
 router
